dffa930cbbed74d16e7fa19f4e3f8d47aa9e57c3
jerry <jerry.jin@mingdao.com>
2014-08-10 07:48:59 +0800

    加入微信昵称与加入收到任务的回复确认

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
-﻿﻿var config = {
+﻿var config = {
 	server: {
-		port: 3000
+		port: 80
 	},
 	redis: {
 		host: '127.0.0.1',
 		port: 6379,
 		storage: {
-			mainUserkey: 'yqby_user',	//	存储用户信息的key，field是用户编号，value同时存储用户发布的需求的编号和被分配到的需求编号，HASH
-			allQueskey: 'yqby_ques',	//	所有存储需求池子的key，field是需求编号，value是需求具体内容，HASH
-			unAssignedQueskey: 'yqby_ques_unas',	//	尚未被分派的需求池子的key，value是需求的编号与发布作者的JSON，LIST
-			assignedQueskey: 'yqby_ques_as',	//	已经被分配的需求池子的key，filed是需求编号，value是被分配的用户编号，HASH
-			unConfirmedQueskey: 'yqby_ques_con',	//	待用户回复确认才发布的任务集合的key，field是用户编号，value是需求具体内容，HASH
-			accesstokenKey: 'yqby_wxat'	//暂存的accessToken，String
+			mainUserkey: 'yqby_user',   //  存储用户信息的key，field是用户编号，value同时存储用户发布的需求的编号和被分配到的需求编号，HASH
+			allQueskey: 'yqby_ques',    //  所有存储需求池子的key，field是需求编号，value是需求具体内容，HASH
+			unAssignedQueskey: 'yqby_ques_unas',    //  尚未被分派的需求池子的key，value是需求的编号与发布作者的JSON，LIST
+			assignedQueskey: 'yqby_ques_as',    //  已经被分配的需求池子的key，filed是需求编号，value是被分配的用户编号，HASH
+			unConfirmedQueskey: 'yqby_ques_uncon',  //  待用户回复确认才发布的任务集合的key，field是用户编号，value是需求具体内容，HASH
+			unAssignedConQueskey: 'yqby_ques_unascon',    //  待用户回复确认才接受的任务集合的key，field是用户编号，value是需求编号和用户编号，HASH
+			accesstokenKey: 'yqby_wxat' //暂存的accessToken，String
 		},
 		//用户上次操作有效时间
 		userlpExpiretime: 5,
@@ -20,17 +21,13 @@
 		//获取的访问令牌有效期
 		accesstokenExpiretime: 60
 	},
-	inputstatus: {
-		default: 0,
-		post_task: 1, // 正在分段发送需求
-		wait_for_deadline: 2 //点击发布完成，等待输入截止时间
-	},
 	weixin: {
 		token: 'xxoo',
 		appkey: 'wx21b41014a26161dd',
 		appsecret: '9c3934bd17bfe6d7292c1c056435709d',
 		api: {
 			access_token: 'https://api.weixin.qq.com/cgi-bin/token',
+			user_detail: 'https://api.weixin.qq.com/cgi-bin/user/info?lang=zh_CN',
 			create_menu: 'https://api.weixin.qq.com/cgi-bin/menu/create',
 			remove_menu: 'https://api.weixin.qq.com/cgi-bin/menu/delete',
 			download_media: 'http://file.api.weixin.qq.com/cgi-bin/media/get',
@@ -47,8 +44,8 @@
 			eventkey31: 'my_task'
 		}
 	},
-	filepath: 'd:/weixin/',
-	logpath: 'd:/log_weixin.txt'
+	filepath: '/usr/local/yqby/log/',
+	logpath: '/usr/local/yqby/log/log_weixin.txt'
 };
 exports.config = config;
 
